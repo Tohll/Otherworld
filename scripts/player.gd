c649cf27b33c337bde0e48f8fc78798496e6022a
Tohll <le.tcharb@gmail.com>
2024-02-18 00:35:19 +0100

    animation linked to movement direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,4 +10,11 @@ func _physics_process(_delta):
 	
 func get_input():
 	var input_direction = Input.get_vector("player_left","player_right","player_up","player_down")
+	input_direction = input_direction.normalized()
 	velocity = input_direction * speed
+	if velocity == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("idle")
+	else:
+		$AnimationTree.get("parameters/playback").travel("run")
+		$AnimationTree.set("parameters/run/blend_position",velocity)
+		$AnimationTree.set("parameters/idle/blend_position",velocity)
