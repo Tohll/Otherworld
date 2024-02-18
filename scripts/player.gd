a886733777800bee6b603bfbbeb46f68d5813248
Tohll <le.tcharb@gmail.com>
2024-02-18 12:01:13 +0100

    basic attack animation & management

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const speed = 200
+var is_attacking = false
 
 func _process(_delta):
 	get_input()
@@ -9,12 +10,22 @@ func _physics_process(_delta):
 	move_and_slide()
 	
 func get_input():
-	var input_direction = Input.get_vector("player_left","player_right","player_up","player_down")
-	input_direction = input_direction.normalized()
-	velocity = input_direction * speed
-	if velocity == Vector2.ZERO:
-		$AnimationTree.get("parameters/playback").travel("idle")
-	else:
-		$AnimationTree.get("parameters/playback").travel("run")
-		$AnimationTree.set("parameters/run/blend_position",velocity)
-		$AnimationTree.set("parameters/idle/blend_position",velocity)
+	if Input.is_action_just_pressed("player_basic_attack"):
+		$AnimationTree.get("parameters/playback").travel("basic_attack")
+		is_attacking = true
+		velocity = Vector2.ZERO
+	if (!is_attacking):
+		var input_direction = Input.get_vector("player_left","player_right","player_up","player_down")
+		input_direction = input_direction.normalized()
+		velocity = input_direction * speed
+		if velocity == Vector2.ZERO:
+			$AnimationTree.get("parameters/playback").travel("idle")
+		else:
+			$AnimationTree.get("parameters/playback").travel("run")
+			$AnimationTree.set("parameters/run/blend_position",velocity)
+			$AnimationTree.set("parameters/idle/blend_position",velocity)
+			$AnimationTree.set("parameters/basic_attack/blend_position",velocity)
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	is_attacking = false
