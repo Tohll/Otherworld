cc1ff98b048b1196507f94b39c9f28985dd95ae1
Franck M <franck@lapinblanc.org>
2024-05-01 21:47:33 -0400

    Spring Clean up first pass
    -First proto has been moved to proto directory
    -old main scene has been renamed to proto (and is still playable)
    -new starting scene is now map_gym in scene/maps/map_gym
    -player has been renamed to hero_warrior and moved to game_objects/characters/heroes folder
    -Goblin warriow has moved to game_objects/characters/enemies folder
    
    all dependencies are now located in the folder of the game object
    ex: goblin warrior contains his sprites in his textures folder
    
    shared dependencies are stored in the _shared folder at the root of character

---
+++ scenes/game_objects/characters/enemies/goblin_warrior/goblin_warrior.gd
@@ -0,0 +1,69 @@
+extends AbsCharacter
+
+func _ready():
+	speed = 100
+	max_life = 10
+	current_life = max_life
+	velocity = Vector2.ZERO
+	damage_indicator_position = Vector2(0,15)
+	damage_range = Vector2(5,15)
+
+func _process(_delta):
+	if !is_dead:
+		set_direction_and_sprite()
+
+func set_direction_and_sprite():
+	if !is_knockbacked:
+		if (is_attacking):
+			show_sprite("attack_sprite")
+			$AnimationTree.get("parameters/playback").travel("attack")
+			return
+		if (player):
+			velocity = position.direction_to(player.position) * speed
+		if velocity == Vector2.ZERO:
+			$AnimationTree.get("parameters/playback").travel("idle")
+			show_sprite("idle_sprite")
+		else:
+			show_sprite("move_sprite")
+			$AnimationTree.get("parameters/playback").travel("walk")
+			$AnimationTree.set("parameters/attack/blend_position",velocity)
+			$AnimationTree.set("parameters/walk/blend_position",velocity)
+			$AnimationTree.set("parameters/idle/blend_position",velocity)
+
+func show_sprite(sprite_name):
+	hide_sprites()
+	match (sprite_name):
+		"attack_sprite":
+			get_node("attack_sprite").show()
+		"move_sprite"	:
+			get_node("move_sprite").show()
+		"idle_sprite":
+			get_node("idle_sprite").show()
+
+func die():
+	queue_free()
+
+func hide_sprites():
+	get_node("attack_sprite").hide()
+	get_node("move_sprite").hide()
+	get_node("idle_sprite").hide()
+
+func _on_detection_body_entered(body):
+	player = body
+
+func _on_detection_body_exited(_body):
+	player = null
+	velocity = Vector2.ZERO
+
+func _on_attack_range_body_entered(_body):
+	is_attacking = true
+
+func _on_attack_range_body_exited(_body):
+	is_attacking = false
+
+func _on_attack_zone_body_entered(body):
+	if !is_dead:
+		body.take_damage(randi_range(damage_range.x,damage_range.y),Vector2.ZERO,"physical")
+
+func _on_knockback_timeout():
+	is_knockbacked = false
--- scripts/main.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _on_button_pressed():
-	get_tree().reload_current_scene()
-	#Commit test
-	#Trigger an open conflict with Charbel**
